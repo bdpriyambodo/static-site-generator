d1fb11ac3d4a6e6d6d8bb22b7046aa8f1e1c3c2e
bdpriyambodo <b.danang.priyambodo@gmail.com>
2025-12-29 20:52:05 +0700

    adding ParentNode class and its unit test

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -8,7 +8,7 @@ class HTMLNode:
         self.props = props
 
     def to_html(self):
-        raise NotImplementedError("Optional error message") 
+        raise NotImplementedError("will be implemented in Child class") 
     
     def props_to_html(self):
         if not self.props:
@@ -29,3 +29,33 @@ class HTMLNode:
             and self.props == other.props
         )    
     
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag, value, children = None, props = props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError('All leaf nodes must have value')
+        if not self.tag:
+            return self.value
+        return (f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>')
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props = None):
+        super().__init__(tag = tag, value = None, children = children, props = props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError('All tags must have value')
+        if not self.children:
+            raise ValueError('All children must have value')
+        for child in self.children:
+            temp = child
+            return (f'<{self.tag}{self.props_to_html()}>{temp.to_html()}</{self.tag}>')
+            # return child.to_html()
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, {self.children}, {self.props})"
