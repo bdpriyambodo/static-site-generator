b14f9e9854ce4d6a12f09f63d85d99a20e9429a2
bdpriyambodo <b.danang.priyambodo@gmail.com>
2025-12-30 14:25:08 +0700

    Moving text_node_to_html_node from to textnode.py & moving unit test to test_textnode.py

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     # PLAIN_TEXT = 'plain text'
@@ -32,3 +33,26 @@ class TextNode:
         return (f'TextNode({self.text}, {self.text_type.value}, {self.url})')
 
 
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text, None )
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text, None )
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text, None )
+        case TextType.CODE:
+            return LeafNode("code", text_node.text, None )
+        case TextType.LINK:
+            prop = {
+                "href": {text_node.url}
+            }
+            return LeafNode("a", text_node.text, prop )
+        case TextType.IMAGE:
+            prop = {
+                "src": text_node.url,
+                "alt": text_node.text          
+            }
+            return LeafNode("img", "", prop )
+        case _:
+            raise Exception('Text Type is not recognized')
